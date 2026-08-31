874fc5015d933915a8e9a45109a35375e721751c
Godwin024 <simondavid5220@gmail.com>
2026-08-31 14:17:47 +0100

    created helpdesk app schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,4 @@
+from .user import UserCreate, UserUpdate, UserResponse
+from .enum import  UserRole, TicketStatus, TicketPriority
+from .ticket import TicketCreate, TicketUpdateStatus, TicketUpdatePriority, TicketResponse
+from .comment import CommentCreate, CommentResponse
